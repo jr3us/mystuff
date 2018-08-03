35ff1e646ede7d6630d19aaa31a3f416fb4087b9
jrh <jrh@jrhimac.local>
2018-08-03 11:08:53 -0400

    add git-cola

--- Raspberry Pi Stuff/New Pi Setup/installpkgs.sh
+++ Raspberry Pi Stuff/New Pi Setup/installpkgs.sh
@@ -1,2 +1,2 @@
-sudo apt-get -y install synaptic vim-gui-common realvnc-vnc-viewer
+sudo apt-get -y install synaptic vim-gui-common realvnc-vnc-viewer git-cola
 sudo apt -y purge wolfram-engine wolframscript
