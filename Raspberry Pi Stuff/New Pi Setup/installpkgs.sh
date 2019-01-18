168c220902e1de306333e16de47b46f40d1f2c84
jr3us <2944683+jr3us@users.noreply.github.com>
2019-01-18 10:18:16 -0500

    add obconf package

--- Raspberry Pi Stuff/New Pi Setup/installpkgs.sh
+++ Raspberry Pi Stuff/New Pi Setup/installpkgs.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install synaptic vim-gui-common realvnc-vnc-viewer git-cola
+sudo apt-get -y install synaptic obconf vim-gui-common realvnc-vnc-viewer git-cola
 sudo apt -y purge wolfram-engine wolframscript
 echo -n " press enter to continue: "
 read ans
