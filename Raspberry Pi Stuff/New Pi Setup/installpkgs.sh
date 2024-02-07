c38f0c4e0ddd70be41c9a203b3170d6b6eef7eb8
jr3us <2944683+jr3us@users.noreply.github.com>
2024-02-07 09:58:44 -0500

    Update installpkgs.sh to accomodate amd64 as well as RPi

--- Raspberry Pi Stuff/New Pi Setup/installpkgs.sh
+++ Raspberry Pi Stuff/New Pi Setup/installpkgs.sh
@@ -1,4 +1,5 @@
-sudo apt-get -y install synaptic obconf vim-gui-common realvnc-vnc-viewer git-cola
+sudo apt-get -y install synaptic obconf vim-gui-common git-cola
+sudo apt -y install realvnc-vnc-viewer
 sudo apt -y purge wolfram-engine wolframscript
 echo -n " press enter to continue: "
 read ans
